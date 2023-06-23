d8447f3226975ca70db6e75c3d1d0c5a63029b7e
laksanai <laksanai.c@e-rong.co.th>
2023-06-23 15:45:03 +0700

    state x array (add)

--- src/App.js
+++ src/App.js
@@ -3,31 +3,77 @@ import './App.css';
 
 function App() {
 
-    const [noteContent, setNoteContent] = useState('')
-    const [noteAuthor, setNoteAuthor] = useState('')
+    //State
+    const [note, setNote] = useState({
+        content:'', author:''
+    });
+    const [allNote, setAllNote] = useState([]);
+
+
+    //Function
+    function onNoteValueChange(event) {
+        const { name, value } = event.target;
+        setNote((prevNote)=>{
+            return {
+                ...prevNote,
+                [name]: value
+            }
+        });
+    }
+
+    function onNoteSubmit(event) {
+        event.preventDefault();
+
+        setAllNote((prevAllNote) =>{
+
+            const newNote = { ...note };
+            newNote.id = Date.now().toString();
+            return [newNote, ...prevAllNote];
+        });
+    }
+
+    //Elements
+    const noteElements = allNote.map((theNote)=>{
+        return (
+            <div key={theNote.id} className='app-note'>
+                <p>{theNote.content}</p>
+                <h5>{theNote.author}</h5>
+            </div>
+        )
+    });
 
 
     return (
         <section className="app-section">
             <div className='app-container'>
-                <h3>สักหน่อยไหมล่ะ</h3>
-                <p>
-                    <input 
-                        type="text"
-                        placeholder="บันทึกความในใจ"
-                        value={noteContent}
-                        onChange={(event)=>{setNoteContent(event.target.value)}}
-                    />
-                </p>
-                <p>
-                    <input 
-                        type="text"
-                        placeholder="ลงชื่อ"
-                        value={noteAuthor}
-                        onChange={(event)=>{setNoteAuthor(event.target.value)}}
-                    />
-                </p>
-
+                <form onSubmit={onNoteSubmit}>
+                    <h3>สักหน่อยไหมล่ะ</h3>
+                    <p>
+                        <textarea
+                            rows="3"
+                            placeholder="บันทึกความในใจ"
+                            name='content'
+                            value={note.content}
+                            onChange={onNoteValueChange}
+                            
+                        />
+                    </p>
+                    <p>
+                        <input 
+                            type="text"
+                            placeholder="ลงชื่อ"
+                            name='author'
+                            value={note.author}
+                            onChange={onNoteValueChange}
+                        />
+                    </p>
+                    <p>
+                        <button type='submit'>เพิ่มข้อมูล</button>
+                    </p>
+                </form>
+                <div className='app-notes'>
+                    {noteElements}
+                </div>
             </div>
         </section>
     );
