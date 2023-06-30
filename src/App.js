a544e0f6836049ffd38a9eee87ffd9ba5881967c
laksanai <laksanai.c@e-rong.co.th>
2023-06-30 17:05:34 +0700

    useState and return

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import {useEffect, useState} from 'react'
 import './App.css';
+import Popup from './Popup';
 
 
 function App() {
 
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        fetch('https://reqres.in/api/users?page=2').then(res => {
-            return res.json();
-
-        }).then(resJson => {
-            console.log(resJson);
-            setUsers(resJson.data);
-        });
-    }, []); 
+    const [isPopupOpen, setIsPopupOpen] = useState(false);
 
 
-    const userElements = users.map((user, index) => {
-        return <h3 key={index}>{user.email}</h3>
-    });
+    let popup = null;
+    if (isPopupOpen) {
+        popup = <Popup onPopupClose={() => setIsPopupOpen(false)} />;
+    }
+
 
     return (
         <section className='app-section'>
             <div className='app-container'>
                 <h1>เก็บโค้ดส่วนลดไม่ทัน</h1>
-                {userElements}
+                <button onClick={() => {setIsPopupOpen(true)}}>Open Popup</button>
+                <p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p>
+                <p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p>
+                <p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p>
+                <p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p>
+                <p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p>
+                <p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p>
+                <p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p>
+                <p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p>
+                <p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p><p>น่ารัก</p>
             </div>
+            {popup}
         </section>
     );
 
